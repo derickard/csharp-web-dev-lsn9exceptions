6d6b6bc4f59c583b3393362e09a4111c03c211fe
derickard <derickard@gmail.com>
2020-06-10 08:51:08 -0400

    exercises complete

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace csharp_web_dev_lsn9exceptions
 {
@@ -7,26 +8,58 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if( y != 0)
+            {
+                return x / y;
+            } else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            string pattern = @"\.cs\z";
+            if(String.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException();
+            }
+            Match m = Regex.Match(fileName, pattern, RegexOptions.IgnoreCase);
+            if (m.Success)
+                return 1;
+            else
+                return 0;
         }
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
-
+            try
+            {
+                Divide(10, 0);
+            } catch (ArgumentOutOfRangeException e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
-
+            try
+            {
+                foreach(KeyValuePair<string, string> s in students)
+                {
+                    Console.WriteLine($"{s.Key} {CheckFileExtension(s.Value)}");
+                    
+                }
+            } 
+            catch (ArgumentNullException e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
 
         }
     }
